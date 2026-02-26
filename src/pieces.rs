163c2a78f411b4fc40ba51cfeb5783d0c0d15fe0
Hedgestock <pamarthur.aito@gmail.com>
2026-02-26 18:59:34 +0700

    Added a piece constructor

--- src/pieces.rs
+++ src/pieces.rs
@@ -27,41 +27,43 @@ pub struct ChessPiece {
     pickable: Pickable,
 }
 
-pub fn chess_piece_factory(
-    piece: PieceType,
-    color: PieceColor,
-    position: board::BoardCoordinates,
-    asset_server: &Res<AssetServer>,
-) -> ChessPiece {
-    let path = format!(
-        "pieces/01_classic/{}-{}.png",
-        if color == PieceColor::White { "w" } else { "b" },
-        match piece {
-            PieceType::Pawn => "pawn",
-            PieceType::Knight => "knight",
-            PieceType::Bishop => "bishop",
-            PieceType::Rook => "rook",
-            PieceType::Queen => "queen",
-            PieceType::King => "king",
-        }
-    );
+impl ChessPiece {
+    pub fn new(
+        piece: PieceType,
+        color: PieceColor,
+        position: board::BoardCoordinates,
+        asset_server: &Res<AssetServer>,
+    ) -> ChessPiece {
+        let path = format!(
+            "pieces/01_classic/{}-{}.png",
+            if color == PieceColor::White { "w" } else { "b" },
+            match piece {
+                PieceType::Pawn => "pawn",
+                PieceType::Knight => "knight",
+                PieceType::Bishop => "bishop",
+                PieceType::Rook => "rook",
+                PieceType::Queen => "queen",
+                PieceType::King => "king",
+            }
+        );
 
-    return ChessPiece {
-        piece,
-        color,
-        sprite: Sprite {
-            image: (asset_server.load(path)),
-            custom_size: Some(Vec2::new(board::SQUARE_SIZE, board::SQUARE_SIZE)),
-            ..default()
-        },
-        transform: Transform::from_xyz(
-            (position.col as f32 - board::BOARD_SIZE as f32 / 2.0) * board::SQUARE_SIZE,
-            (position.row as f32 - board::BOARD_SIZE as f32 / 2.0) * board::SQUARE_SIZE,
-            1.,
-        ),
-        pickable: Pickable {
-            should_block_lower: false,
-            ..default()
-        },
-    };
+        return ChessPiece {
+            piece,
+            color,
+            sprite: Sprite {
+                image: (asset_server.load(path)),
+                custom_size: Some(Vec2::new(board::SQUARE_SIZE, board::SQUARE_SIZE)),
+                ..default()
+            },
+            transform: Transform::from_xyz(
+                (position.col as f32 - board::BOARD_SIZE as f32 / 2.0) * board::SQUARE_SIZE,
+                (position.row as f32 - board::BOARD_SIZE as f32 / 2.0) * board::SQUARE_SIZE,
+                1.,
+            ),
+            pickable: Pickable {
+                should_block_lower: false,
+                ..default()
+            },
+        };
+    }
 }
