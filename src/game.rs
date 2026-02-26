3d281e0c59058dca99a34baadcca37592568a471
Hedgestock <pamarthur.aito@gmail.com>
2026-02-26 18:01:48 +0700

    Added basic bitboard operations

---
+++ src/game.rs
@@ -0,0 +1,274 @@
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Mul, Not};
+
+use bevy::ecs::resource::Resource;
+
+#[derive(Debug)]
+pub struct BitBoard(pub u64);
+
+impl BitAnd for BitBoard {
+    type Output = BitBoard;
+
+    fn bitand(self, other: BitBoard) -> BitBoard {
+        return  BitBoard(self.0 & other.0)
+    }
+}
+
+impl BitAnd for &BitBoard {
+    type Output = BitBoard;
+
+    fn bitand(self, other: &BitBoard) -> BitBoard {
+        return  BitBoard(self.0 & other.0)
+    }
+}
+
+impl BitAnd<&BitBoard> for BitBoard {
+    type Output = BitBoard;
+
+    fn bitand(self, other: &BitBoard) -> BitBoard {
+        return  BitBoard(self.0 & other.0)
+    }
+}
+
+impl BitAnd<BitBoard> for &BitBoard {
+    type Output = BitBoard;
+
+    fn bitand(self, other: BitBoard) -> BitBoard {
+        return  BitBoard(self.0 & other.0)
+    }
+}
+
+// Impl BitOr
+impl BitOr for BitBoard {
+    type Output = BitBoard;
+
+    fn bitor(self, other: BitBoard) -> BitBoard {
+        return  BitBoard(self.0 | other.0)
+    }
+}
+
+impl BitOr for &BitBoard {
+    type Output = BitBoard;
+
+    fn bitor(self, other: &BitBoard) -> BitBoard {
+        return  BitBoard(self.0 | other.0)
+    }
+}
+
+impl BitOr<&BitBoard> for BitBoard {
+    type Output = BitBoard;
+
+    fn bitor(self, other: &BitBoard) -> BitBoard {
+        return  BitBoard(self.0 | other.0)
+    }
+}
+
+impl BitOr<BitBoard> for &BitBoard {
+    type Output = BitBoard;
+
+    fn bitor(self, other: BitBoard) -> BitBoard {
+        return  BitBoard(self.0 | other.0)
+    }
+}
+
+// Impl BitXor
+
+impl BitXor for BitBoard {
+    type Output = BitBoard;
+
+    fn bitxor(self, other: BitBoard) -> BitBoard {
+        return  BitBoard(self.0 ^ other.0)
+    }
+}
+
+impl BitXor for &BitBoard {
+    type Output = BitBoard;
+
+    fn bitxor(self, other: &BitBoard) -> BitBoard {
+        return  BitBoard(self.0 ^ other.0)
+    }
+}
+
+impl BitXor<&BitBoard> for BitBoard {
+    type Output = BitBoard;
+
+    fn bitxor(self, other: &BitBoard) -> BitBoard {
+        return  BitBoard(self.0 ^ other.0)
+    }
+}
+
+impl BitXor<BitBoard> for &BitBoard {
+    type Output = BitBoard;
+
+    fn bitxor(self, other: BitBoard) -> BitBoard {
+        return  BitBoard(self.0 ^ other.0)
+    }
+}
+
+// Impl BitAndAssign
+
+impl BitAndAssign for BitBoard {
+    fn bitand_assign(&mut self, other: BitBoard) {
+        self.0 &= other.0;
+    }
+}
+
+impl BitAndAssign<&BitBoard> for BitBoard {
+    fn bitand_assign(&mut self, other: &BitBoard) {
+        self.0 &= other.0;
+    }
+}
+
+// Impl BitOrAssign
+impl BitOrAssign for BitBoard {
+    fn bitor_assign(&mut self, other: BitBoard) {
+        self.0 |= other.0;
+    }
+}
+
+impl BitOrAssign<&BitBoard> for BitBoard {
+    fn bitor_assign(&mut self, other: &BitBoard) {
+        self.0 |= other.0;
+    }
+}
+
+// Impl BitXor Assign
+impl BitXorAssign for BitBoard {
+    fn bitxor_assign(&mut self, other: BitBoard) {
+        self.0 ^= other.0;
+    }
+}
+
+impl BitXorAssign<&BitBoard> for BitBoard {
+    fn bitxor_assign(&mut self, other: &BitBoard) {
+        self.0 ^= other.0;
+    }
+}
+
+// Impl Mul
+impl Mul for BitBoard {
+    type Output = BitBoard;
+
+    fn mul(self, other: BitBoard) -> BitBoard {
+        return  BitBoard(self.0.wrapping_mul(other.0))
+    }
+}
+
+impl Mul for &BitBoard {
+    type Output = BitBoard;
+
+    fn mul(self, other: &BitBoard) -> BitBoard {
+        return  BitBoard(self.0.wrapping_mul(other.0))
+    }
+}
+
+impl Mul<&BitBoard> for BitBoard {
+    type Output = BitBoard;
+
+    fn mul(self, other: &BitBoard) -> BitBoard {
+        return  BitBoard(self.0.wrapping_mul(other.0))
+    }
+}
+
+impl Mul<BitBoard> for &BitBoard {
+    type Output = BitBoard;
+
+    fn mul(self, other: BitBoard) -> BitBoard {
+        return  BitBoard(self.0.wrapping_mul(other.0))
+    }
+}
+
+// Impl Not
+impl Not for BitBoard {
+    type Output = BitBoard;
+
+    fn not(self) -> BitBoard {
+        BitBoard(!self.0)
+    }
+}
+
+impl Not for &BitBoard {
+    type Output = BitBoard;
+
+    fn not(self) -> BitBoard {
+        BitBoard(!self.0)
+    }
+}
+
+#[derive(Resource, Debug)]
+pub struct GameState {
+    pub white_pawn: BitBoard,
+    pub black_pawn: BitBoard,
+    pub white_knight: BitBoard,
+    pub black_knight: BitBoard,
+    pub white_bishop: BitBoard,
+    pub black_bishop: BitBoard,
+    pub white_rook: BitBoard,
+    pub black_rook: BitBoard,
+    pub white_queen: BitBoard,
+    pub black_queen: BitBoard,
+    pub white_king: BitBoard,
+    pub black_king: BitBoard,
+}
+
+impl GameState {
+    pub fn white_pieces(&self) -> BitBoard {
+        return &self.white_pawn
+            | &self.white_knight
+            | &self.white_bishop
+            | &self.white_rook
+            | &self.white_queen
+            | &self.white_king;
+    }
+
+    pub fn black_pieces(&self) -> BitBoard {
+        return &self.black_pawn
+            | &self.black_knight
+            | &self.black_bishop
+            | &self.black_rook
+            | &self.black_queen
+            | &self.black_king;
+    }
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        GameState {
+            white_pawn: BitBoard(
+                0b00000000_00000000_00000000_00000000_00000000_00000000_11111111_00000000,
+            ),
+            black_pawn: BitBoard(
+                0b00000000_11111111_00000000_00000000_00000000_00000000_00000000_00000000,
+            ),
+            white_knight: BitBoard(
+                0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_01000010,
+            ),
+            black_knight: BitBoard(
+                0b01000010_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+            ),
+            white_bishop: BitBoard(
+                0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00100100,
+            ),
+            black_bishop: BitBoard(
+                0b00100100_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+            ),
+            white_rook: BitBoard(
+                0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_10000001,
+            ),
+            black_rook: BitBoard(
+                0b10000001_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+            ),
+            white_queen: BitBoard(
+                0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00001000,
+            ),
+            black_queen: BitBoard(
+                0b00001000_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+            ),
+            white_king: BitBoard(
+                0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00010000,
+            ),
+            black_king: BitBoard(
+                0b00010000_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+            ),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod board;
 mod pieces;
+mod game;
 
 use bevy::{picking::hover::PickingInteraction, prelude::*};
 use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
@@ -14,124 +15,128 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin::default())
         .add_plugins(WorldInspectorPlugin::new())
+        .init_resource::<game::GameState>()
         .add_systems(Startup, (setup, board::setup))
         .add_systems(Update, hover_system)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, game_state: Res<game::GameState>) {
     commands.spawn(Camera2d);
 
-    for i in 0..8 {
-        commands.spawn(chess_piece_factory(
-            PieceType::Pawn,
-            PieceColor::White,
-            BoardCoordinates { col: i, row: 1 },
-            &asset_server,
-        ));
-        commands.spawn(chess_piece_factory(
-            PieceType::Pawn,
-            PieceColor::Black,
-            BoardCoordinates { col: i, row: 6 },
-            &asset_server,
-        ));
-    }
-    commands.spawn(chess_piece_factory(
-        PieceType::Rook,
-        PieceColor::White,
-        BoardCoordinates { col: 0, row: 0 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Rook,
-        PieceColor::Black,
-        BoardCoordinates { col: 0, row: 7 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Rook,
-        PieceColor::White,
-        BoardCoordinates { col: 7, row: 0 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Rook,
-        PieceColor::Black,
-        BoardCoordinates { col: 7, row: 7 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Knight,
-        PieceColor::White,
-        BoardCoordinates { col: 1, row: 0 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Knight,
-        PieceColor::Black,
-        BoardCoordinates { col: 1, row: 7 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Knight,
-        PieceColor::White,
-        BoardCoordinates { col: 6, row: 0 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Knight,
-        PieceColor::Black,
-        BoardCoordinates { col: 6, row: 7 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Bishop,
-        PieceColor::White,
-        BoardCoordinates { col: 2, row: 0 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Bishop,
-        PieceColor::Black,
-        BoardCoordinates { col: 2, row: 7 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Bishop,
-        PieceColor::White,
-        BoardCoordinates { col: 5, row: 0 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Bishop,
-        PieceColor::Black,
-        BoardCoordinates { col: 5, row: 7 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Queen,
-        PieceColor::White,
-        BoardCoordinates { col: 4, row: 0 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::Queen,
-        PieceColor::Black,
-        BoardCoordinates { col: 4, row: 7 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::King,
-        PieceColor::White,
-        BoardCoordinates { col: 3, row: 0 },
-        &asset_server,
-    ));
-    commands.spawn(chess_piece_factory(
-        PieceType::King,
-        PieceColor::Black,
-        BoardCoordinates { col: 3, row: 7 },
-        &asset_server,
-    ));
+    println!("{:?}", game_state.white_pieces())
+    println!("{:?}", game_state.black_pieces())
+
+    // for i in 0..8 {
+    //     commands.spawn(chess_piece_factory(
+    //         PieceType::Pawn,
+    //         PieceColor::White,
+    //         BoardCoordinates { col: i, row: 1 },
+    //         &asset_server,
+    //     ));
+    //     commands.spawn(chess_piece_factory(
+    //         PieceType::Pawn,
+    //         PieceColor::Black,
+    //         BoardCoordinates { col: i, row: 6 },
+    //         &asset_server,
+    //     ));
+    // }
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Rook,
+    //     PieceColor::White,
+    //     BoardCoordinates { col: 0, row: 0 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Rook,
+    //     PieceColor::Black,
+    //     BoardCoordinates { col: 0, row: 7 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Rook,
+    //     PieceColor::White,
+    //     BoardCoordinates { col: 7, row: 0 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Rook,
+    //     PieceColor::Black,
+    //     BoardCoordinates { col: 7, row: 7 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Knight,
+    //     PieceColor::White,
+    //     BoardCoordinates { col: 1, row: 0 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Knight,
+    //     PieceColor::Black,
+    //     BoardCoordinates { col: 1, row: 7 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Knight,
+    //     PieceColor::White,
+    //     BoardCoordinates { col: 6, row: 0 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Knight,
+    //     PieceColor::Black,
+    //     BoardCoordinates { col: 6, row: 7 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Bishop,
+    //     PieceColor::White,
+    //     BoardCoordinates { col: 2, row: 0 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Bishop,
+    //     PieceColor::Black,
+    //     BoardCoordinates { col: 2, row: 7 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Bishop,
+    //     PieceColor::White,
+    //     BoardCoordinates { col: 5, row: 0 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Bishop,
+    //     PieceColor::Black,
+    //     BoardCoordinates { col: 5, row: 7 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Queen,
+    //     PieceColor::White,
+    //     BoardCoordinates { col: 4, row: 0 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::Queen,
+    //     PieceColor::Black,
+    //     BoardCoordinates { col: 4, row: 7 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::King,
+    //     PieceColor::White,
+    //     BoardCoordinates { col: 3, row: 0 },
+    //     &asset_server,
+    // ));
+    // commands.spawn(chess_piece_factory(
+    //     PieceType::King,
+    //     PieceColor::Black,
+    //     BoardCoordinates { col: 3, row: 7 },
+    //     &asset_server,
+    // ));
 }
 
 fn hover_system(
