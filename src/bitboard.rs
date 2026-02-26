9097c8447c05a612824d6a0f564ad77dd7bb1159
Hedgestock <pamarthur.aito@gmail.com>
2026-02-26 21:33:27 +0700

    Brain goes boom from macros

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,5 +1,7 @@
-macro_rules! impl_bit_op {
-    ($trait:ident, $method:ident, $assign_trait:ident, $assign_method:ident) => {
+macro_rules! impl_bit_ops {
+    ($(($trait:ident, $method:ident, $assign_trait:ident, $assign_method:ident)),*) => {
+        $(
+
         // --- Value and Reference Permutations ---
         // BitBoard op BitBoard
         impl std::ops::$trait<BitBoard> for BitBoard {
@@ -31,18 +33,19 @@ macro_rules! impl_bit_op {
         impl std::ops::$assign_trait<&BitBoard> for BitBoard {
             #[inline] fn $assign_method(&mut self, rhs: &BitBoard) { self.0.$assign_method(rhs.0); }
         }
+        )*
     };
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct BitBoard( 
-    pub u64
-);
+pub struct BitBoard(pub u64);
 
-impl_bit_op!(BitAnd, bitand, BitAndAssign, bitand_assign);
-impl_bit_op!(BitOr,  bitor,  BitOrAssign,  bitor_assign);
-impl_bit_op!(BitXor, bitxor, BitXorAssign, bitxor_assign);
-impl_bit_op!(Add, add, AddAssign, add_assign);
-impl_bit_op!(Sub, sub, SubAssign, sub_assign);
-impl_bit_op!(Mul, mul, MulAssign, mul_assign);
-impl_bit_op!(Div, div, DivAssign, div_assign);
+impl_bit_ops!(
+    (BitAnd, bitand, BitAndAssign, bitand_assign),
+    (BitOr, bitor, BitOrAssign, bitor_assign),
+    (BitXor, bitxor, BitXorAssign, bitxor_assign),
+    (Add, add, AddAssign, add_assign),
+    (Sub, sub, SubAssign, sub_assign),
+    (Mul, mul, MulAssign, mul_assign),
+    (Div, div, DivAssign, div_assign)
+);
