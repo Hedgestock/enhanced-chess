cde14a93a1395f5bde942bb20d7eb8abcebb3e15
Hedgestock <pamarthur.aito@gmail.com>
2026-03-08 12:07:30 +0700

    WIP: fun with macros

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -3,38 +3,89 @@ use std::fmt;
 macro_rules! impl_bit_ops {
     ($(($trait:ident, $method:ident, $assign_trait:ident, $assign_method:ident)),*) => {
         $(
+            // --- Value and Reference Permutations ---
+            // BitBoard op BitBoard
+            impl std::ops::$trait<BitBoard> for BitBoard {
+                type Output = BitBoard;
+                #[inline] fn $method(self, rhs: BitBoard) -> BitBoard { BitBoard(self.0.$method(rhs.0)) }
+            }
+            // &BitBoard op BitBoard
+            impl std::ops::$trait<BitBoard> for &BitBoard {
+                type Output = BitBoard;
+                #[inline] fn $method(self, rhs: BitBoard) -> BitBoard { BitBoard(self.0.$method(rhs.0)) }
+            }
+            // BitBoard op &BitBoard
+            impl std::ops::$trait<&BitBoard> for BitBoard {
+                type Output = BitBoard;
+                #[inline] fn $method(self, rhs: &BitBoard) -> BitBoard { BitBoard(self.0.$method(rhs.0)) }
+            }
+            // &BitBoard op &BitBoard
+            impl std::ops::$trait<&BitBoard> for &BitBoard {
+                type Output = BitBoard;
+                #[inline] fn $method(self, rhs: &BitBoard) -> BitBoard { BitBoard(self.0.$method(rhs.0)) }
+            }
 
-        // --- Value and Reference Permutations ---
-        // BitBoard op BitBoard
-        impl std::ops::$trait<BitBoard> for BitBoard {
-            type Output = BitBoard;
-            #[inline] fn $method(self, rhs: BitBoard) -> BitBoard { BitBoard(self.0.$method(rhs.0)) }
-        }
-        // &BitBoard op BitBoard
-        impl std::ops::$trait<BitBoard> for &BitBoard {
-            type Output = BitBoard;
-            #[inline] fn $method(self, rhs: BitBoard) -> BitBoard { BitBoard(self.0.$method(rhs.0)) }
-        }
-        // BitBoard op &BitBoard
-        impl std::ops::$trait<&BitBoard> for BitBoard {
-            type Output = BitBoard;
-            #[inline] fn $method(self, rhs: &BitBoard) -> BitBoard { BitBoard(self.0.$method(rhs.0)) }
-        }
-        // &BitBoard op &BitBoard
-        impl std::ops::$trait<&BitBoard> for &BitBoard {
-            type Output = BitBoard;
-            #[inline] fn $method(self, rhs: &BitBoard) -> BitBoard { BitBoard(self.0.$method(rhs.0)) }
-        }
+            // --- Assignment Permutations ---
+            // BitBoard op= BitBoard
+            impl std::ops::$assign_trait<BitBoard> for BitBoard {
+                #[inline] fn $assign_method(&mut self, rhs: BitBoard) { self.0.$assign_method(rhs.0); }
+            }
+            // BitBoard op= &BitBoard
+            impl std::ops::$assign_trait<&BitBoard> for BitBoard {
+                #[inline] fn $assign_method(&mut self, rhs: &BitBoard) { self.0.$assign_method(rhs.0); }
+            }
 
-        // --- Assignment Permutations ---
-        // BitBoard op= BitBoard
-        impl std::ops::$assign_trait<BitBoard> for BitBoard {
-            #[inline] fn $assign_method(&mut self, rhs: BitBoard) { self.0.$assign_method(rhs.0); }
-        }
-        // BitBoard op= &BitBoard
-        impl std::ops::$assign_trait<&BitBoard> for BitBoard {
-            #[inline] fn $assign_method(&mut self, rhs: &BitBoard) { self.0.$assign_method(rhs.0); }
-        }
+            impl_unsigned_bit_ops!($trait, $method, $assign_trait, $assign_method);
+        )*
+    };
+}
+
+macro_rules! impl_unsigned_bit_ops {
+    ($trait:ident, $method:ident, $assign_trait:ident, $assign_method:ident) => {
+        impl_unsigned_bit_ops_for!($trait, $method, $assign_trait, $assign_method;
+            u8, u16, u32, u64, u128, usize
+        );
+    };
+}
+
+macro_rules! impl_unsigned_bit_ops_for {
+    ($trait:ident, $method:ident, $assign_trait:ident, $assign_method:ident; $($t:ty),*) => {
+        $(
+            // --- Value and Reference Permutations ---
+            // BitBoard op t
+            impl std::ops::$trait<$t> for BitBoard {
+                type Output = BitBoard;
+                #[inline] fn $method(self, rhs: $t) -> BitBoard { BitBoard(self.0.$method(rhs as u64)) }
+            }
+
+            // &BitBoard op t
+            impl std::ops::$trait<$t> for &BitBoard {
+                type Output = BitBoard;
+                #[inline] fn $method(self, rhs: $t) -> BitBoard { BitBoard(self.0.$method(rhs as u64)) }
+            }
+
+            // BitBoard op &t
+            impl std::ops::$trait<&$t> for BitBoard {
+                type Output = BitBoard;
+                #[inline] fn $method(self, rhs: &$t) -> BitBoard { BitBoard(self.0.$method(*rhs as u64)) }
+            }
+
+            // &BitBoard op &t
+            impl std::ops::$trait<&$t> for &BitBoard {
+                type Output = BitBoard;
+                #[inline] fn $method(self, rhs: &$t) -> BitBoard { BitBoard(self.0.$method(*rhs as u64)) }
+            }
+
+            // --- Assignment Permutations ---
+            // BitBoard op= t
+            impl std::ops::$assign_trait<$t> for BitBoard {
+                #[inline] fn $assign_method(&mut self, rhs: $t) { self.0.$assign_method(rhs as u64); }
+            }
+            
+            // BitBoard op= &t
+            impl std::ops::$assign_trait<&$t> for BitBoard {
+                #[inline] fn $assign_method(&mut self, rhs: &$t) { self.0.$assign_method(*rhs as u64); }
+            }
         )*
     };
 }
@@ -116,6 +167,30 @@ mod tests {
     }
 
     #[test]
+    fn bb_and_bb() {
+        let bblhs = BitBoard(5);
+        let bbrhs = BitBoard(9);
+        let bbres = BitBoard(1);
+        assert_eq!(bblhs & bbrhs, bbres);
+    }
+
+    #[test]
+    fn bb_or_bb() {
+        let bblhs = BitBoard(5);
+        let bbrhs = BitBoard(10);
+        let bbres = BitBoard(15);
+        assert_eq!(bblhs | bbrhs, bbres);
+    }
+
+    #[test]
+    fn bb_xor_bb() {
+        let bblhs = BitBoard(9);
+        let bbrhs = BitBoard(10);
+        let bbres = BitBoard(3);
+        assert_eq!(bblhs ^ bbrhs, bbres);
+    }
+
+    #[test]
     fn rbb_sub_bb() {
         let bblhs = BitBoard(6);
         let bbrhs = BitBoard(2);
@@ -139,12 +214,36 @@ mod tests {
         assert_eq!(&bblhs / bbrhs, bbres);
     }
 
-        #[test]
+    #[test]
     fn bb_add_rbb() {
         let bblhs = BitBoard(6);
         let bbrhs = BitBoard(2);
         let bbres = BitBoard(8);
         assert_eq!(bblhs + &bbrhs, bbres);
+    }
+
+    #[test]
+    fn rbb_and_bb() {
+        let bblhs = BitBoard(5);
+        let bbrhs = BitBoard(9);
+        let bbres = BitBoard(1);
+        assert_eq!(&bblhs & bbrhs, bbres);
+    }
+
+    #[test]
+    fn rbb_or_bb() {
+        let bblhs = BitBoard(5);
+        let bbrhs = BitBoard(10);
+        let bbres = BitBoard(15);
+        assert_eq!(&bblhs | bbrhs, bbres);
+    }
+
+    #[test]
+    fn rbb_xor_bb() {
+        let bblhs = BitBoard(9);
+        let bbrhs = BitBoard(10);
+        let bbres = BitBoard(3);
+        assert_eq!(&bblhs ^ bbrhs, bbres);
     }
 
     #[test]
@@ -171,7 +270,31 @@ mod tests {
         assert_eq!(bblhs / &bbrhs, bbres);
     }
 
-        #[test]
+    #[test]
+    fn bb_and_rbb() {
+        let bblhs = BitBoard(5);
+        let bbrhs = BitBoard(9);
+        let bbres = BitBoard(1);
+        assert_eq!(bblhs & &bbrhs, bbres);
+    }
+
+    #[test]
+    fn bb_or_rbb() {
+        let bblhs = BitBoard(5);
+        let bbrhs = BitBoard(10);
+        let bbres = BitBoard(15);
+        assert_eq!(bblhs | &bbrhs, bbres);
+    }
+
+    #[test]
+    fn bb_xor_rbb() {
+        let bblhs = BitBoard(9);
+        let bbrhs = BitBoard(10);
+        let bbres = BitBoard(3);
+        assert_eq!(bblhs ^ &bbrhs, bbres);
+    }
+
+    #[test]
     fn rbb_add_rbb() {
         let bblhs = BitBoard(6);
         let bbrhs = BitBoard(2);
@@ -201,5 +324,164 @@ mod tests {
         let bbrhs = BitBoard(2);
         let bbres = BitBoard(3);
         assert_eq!(&bblhs / &bbrhs, bbres);
+    }
+
+    #[test]
+    fn rbb_and_rbb() {
+        let bblhs = BitBoard(5);
+        let bbrhs = BitBoard(9);
+        let bbres = BitBoard(1);
+        assert_eq!(&bblhs & &bbrhs, bbres);
+    }
+
+    #[test]
+    fn rbb_or_rbb() {
+        let bblhs = BitBoard(5);
+        let bbrhs = BitBoard(10);
+        let bbres = BitBoard(15);
+        assert_eq!(&bblhs | &bbrhs, bbres);
+    }
+
+    #[test]
+    fn rbb_xor_rbb() {
+        let bblhs = BitBoard(9);
+        let bbrhs = BitBoard(10);
+        let bbres = BitBoard(3);
+        assert_eq!(&bblhs ^ &bbrhs, bbres);
+    }
+
+    #[test]
+    fn bb_add_assign_bb() {
+        let mut bblhs = BitBoard(6);
+        bblhs += BitBoard(2);
+        let bbres = BitBoard(8);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_sub_assign_bb() {
+        let mut bblhs = BitBoard(6);
+        bblhs -= BitBoard(2);
+        let bbres = BitBoard(4);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_mul_assign_bb() {
+        let mut bblhs = BitBoard(6);
+        bblhs *= BitBoard(2);
+        let bbres = BitBoard(12);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_div_assign_bb() {
+        let mut bblhs = BitBoard(6);
+        bblhs /= BitBoard(2);
+        let bbres = BitBoard(3);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_and_assign_bb() {
+        let mut bblhs = BitBoard(5);
+        bblhs &= BitBoard(9);
+        let bbres = BitBoard(1);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_or_assign_bb() {
+        let mut bblhs = BitBoard(5);
+        bblhs |= BitBoard(10);
+        let bbres = BitBoard(15);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_xor_assign_bb() {
+        let mut bblhs = BitBoard(9);
+        bblhs ^= BitBoard(10);
+        let bbres = BitBoard(3);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_add_assign_rbb() {
+        let mut bblhs = BitBoard(6);
+        let bbrhs = BitBoard(2);
+        bblhs += &bbrhs;
+        let bbres = BitBoard(8);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_sub_assign_rbb() {
+        let mut bblhs = BitBoard(6);
+        let bbrhs = BitBoard(2);
+        bblhs -= &bbrhs;
+        let bbres = BitBoard(4);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_mul_assign_rbb() {
+        let mut bblhs = BitBoard(6);
+        let bbrhs = BitBoard(2);
+        bblhs *= &bbrhs;
+        let bbres = BitBoard(12);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_div_assign_rbb() {
+        let mut bblhs = BitBoard(6);
+        let bbrhs = BitBoard(2);
+        bblhs /= &bbrhs;
+        let bbres = BitBoard(3);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_and_assign_rbb() {
+        let mut bblhs = BitBoard(5);
+        let bbrhs = BitBoard(9);
+        bblhs &= &bbrhs;
+        let bbres = BitBoard(1);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_or_assign_rbb() {
+        let mut bblhs = BitBoard(5);
+        let bbrhs = BitBoard(10);
+        bblhs |= &bbrhs;
+        let bbres = BitBoard(15);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_xor_assign_rbb() {
+        let mut bblhs = BitBoard(9);
+        let bbrhs = BitBoard(10);
+        bblhs ^= &bbrhs;
+        let bbres = BitBoard(3);
+        assert_eq!(bblhs, bbres);
+    }
+
+    #[test]
+    fn bb_add_int() {
+        let bblhs = BitBoard(6);
+        let bbrhs = 2;
+        let bbres = BitBoard(8);
+        assert_eq!(bblhs + bbrhs, bbres);
+    }
+
+    #[test]
+    fn int_add_bb() {
+        let bblhs = BitBoard(6);
+        let bbrhs = 2;
+        let bbres = BitBoard(8);
+        assert_eq!(bblhs + bbrhs, bbres);
     }
 }
